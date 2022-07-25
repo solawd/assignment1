e364cfca24f7289fbe06e859b0d134cc049d9d1c
solawd <solawd@gmail.com>
2022-07-25 01:50:02 +0000

    Add on conflict to insert statements

--- sql_queries.py
+++ sql_queries.py
@@ -62,6 +62,7 @@ INSERT INTO song_plays (start_time, user_id, level, song_id, artist_id, session_
 user_table_insert = ("""
 INSERT INTO users (user_id, first_name, last_name, gender, level) 
                  VALUES (%s, %s, %s, %s, %s)
+                 ON CONFLICT (user_id) DO UPDATE SET level=EXCLUDED.level
 """)
 
 song_table_insert = ("""
@@ -72,12 +73,14 @@ INSERT INTO songs (song_id, title, artist_id, year, duration)
 artist_table_insert = ("""
 INSERT INTO artists (artist_id, name, location, latitude, longitude) 
                  VALUES (%s, %s, %s, %s, %s)
+                 ON CONFLICT DO NOTHING
 """)
 
 
 time_table_insert = ("""
 INSERT INTO times (start_time, hour, day, week, month, year, weekday) 
                  VALUES (%s, %s, %s, %s, %s, %s, %s)
+                 ON CONFLICT DO NOTHING
 """)
 
 # FIND SONGS
